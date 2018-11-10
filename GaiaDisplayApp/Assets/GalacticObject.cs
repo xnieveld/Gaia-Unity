12ba030dfead1bb9df2dada5c2f4a4c1c9ac2816
danny <dannydorstijn1997@gmail.com>
2018-11-10 11:34:45 +0100

    Defined GalacticObject and worked on binary format test

--- GaiaDisplayApp/Assets/GalacticObject.cs
+++ GaiaDisplayApp/Assets/GalacticObject.cs
@@ -1,28 +1,11 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GalacticObject : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
-    public Vector3 GetPosition() {
-        return;   
-    }
-
-    public float GetAbsoluteMagnitude() {
-        return;
-    }
-
-    public float GetBoundingSphereRadius() {
-        return;
-    }
+[Serializable]
+public class GalacticObject {
+    public Vector3 coordinates;
+    public Vector4 colour;
+    public float magnitude;
 }
