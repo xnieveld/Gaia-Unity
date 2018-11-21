cc43bd2223fc29cd195de5bfde6c36fec645fc79
danny <dannydorstijn1997@gmail.com>
2018-11-21 10:05:08 +0100

    added updated library

---
+++ GaiaDisplayApp/Assets/OctreeDLL.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Runtime.InteropServices;
+using System;
+
+class OctreeDLL : MonoBehaviour
+{
+    // The imported function
+    [DllImport("liboctree", EntryPoint = "oct_octree_init")]
+    public static extern IntPtr InitOctree(float[] position, long size);
+
+    [DllImport("liboctree", EntryPoint = "oct_octree_build")]
+    public static extern void BuildOctree(IntPtr octree, float[][] object_positions, long object_count);
+
+    public int[] a;
+
+    void Start()
+    {
+        float[] position = { 100, 100, 100 };
+        IntPtr octree = InitOctree(position, 100);
+
+        float[] pos1 = new float[3] { 1, 2, 3 };
+        float[] pos2 = new float[3] { 5, 6, 7 };
+        float[] pos3 = new float[3] { 1, 3, 2 };
+        float[] pos4 = new float[3] { 5, 4, 3 };
+        float[][] objects = { pos1, pos2, pos3, pos4 };
+
+        BuildOctree(octree, objects, objects.Length);
+    }
+}
