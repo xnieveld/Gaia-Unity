efb1730dc327b68325d226bb3b13dd26c999bfb7
ddorstijn <dannydorstijn1997@gmail.com>
2018-11-21 18:12:17 +0100

    Work on octree rspper

--- GaiaDisplayApp/Assets/OctreeDLL.cs
+++ GaiaDisplayApp/Assets/OctreeDLL.cs
@@ -5,11 +5,14 @@ using System;
 class OctreeDLL : MonoBehaviour
 {
     // The imported function
-    [DllImport("liboctree", EntryPoint = "oct_octree_init")]
-    public static extern IntPtr InitOctree(float[] position, long size);
+    [DllImport("octree", EntryPoint = "oct_octree_init")]
+    public static extern IntPtr InitOctree([In, Out] float[] position, long size);
 
-    [DllImport("liboctree", EntryPoint = "oct_octree_build")]
+    [DllImport("octree", EntryPoint = "oct_octree_build")]
     public static extern void BuildOctree(IntPtr octree, float[,] object_positions, long object_count);
+
+    [DllImport("octree", EntryPoint = "oct_octree_free")]
+    public static extern void FreeOctree(IntPtr octree);
 
     void Start()
     {
@@ -18,6 +21,8 @@ class OctreeDLL : MonoBehaviour
 
         float[,] objects = { {1,5,2}, {4,6,1}, {8,9,4}, {5,7,3} };
 
-        BuildOctree(octree, objects, objects.Length);
+        /* BuildOctree(octree, objects, objects.Length); */
+
+        FreeOctree(octree);
     }
 }
