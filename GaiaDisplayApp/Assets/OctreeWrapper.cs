519236090e6e07876ad73ff7acf7c8c4f71d36a7
danny <dannydorstijn1997@gmail.com>
2018-11-23 01:53:05 +0100

    Added more function and rename of OctreeDLL to OctreeWrapper

---
+++ GaiaDisplayApp/Assets/OctreeWrapper.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Runtime.InteropServices;
+using System;
+
+class OctreeWrapper : MonoBehaviour
+{
+    // The imported function
+    [DllImport("octree", EntryPoint = "oct_octree_init", CharSet = CharSet.Unicode)]
+    public static extern IntPtr InitOctree(Position position, uint size);
+
+    [DllImport("octree", EntryPoint = "oct_octree_build", CharSet = CharSet.Unicode)]
+    public static extern void BuildOctree(IntPtr octree, Position[] object_positions, uint object_count);
+
+    [DllImport("octree", EntryPoint = "oct_octree_free", CharSet = CharSet.Unicode)]
+    public static extern void FreeOctree(IntPtr octree);
+
+    [DllImport("octree", EntryPoint = "oct_octree_get_size", CharSet = CharSet.Unicode)]
+    public static extern uint GetSize(IntPtr octree);
+
+    [DllImport("octree", EntryPoint = "oct_octree_get_inner_count", CharSet = CharSet.Unicode)]
+    public static extern uint GetInnerCount(IntPtr octree);
+
+    [DllImport("octree", EntryPoint = "oct_octree_get_leaf_count", CharSet = CharSet.Unicode)]
+    public static extern uint GetLeafCount(IntPtr octree);
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct Position
+    {
+        public float x;
+        public float y;
+        public float z;
+
+        public Position(float x, float y, float z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+    }
+
+    IntPtr octree;
+
+    void Start()
+    {
+        Position position = new Position(100, 100, 100);
+        octree = InitOctree(position, 100);
+
+        Position[] objects = new Position[5];
+        for (int i = 0; i < objects.Length; i++)
+        {
+            objects[i].x = UnityEngine.Random.Range(0, 10f);
+            objects[i].y = UnityEngine.Random.Range(0, 10f);
+            objects[i].z = UnityEngine.Random.Range(0, 10f);
+        }
+
+        BuildOctree(octree, objects, (uint)objects.Length);
+
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log(GetSize(octree));
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            Debug.Log(GetInnerCount(octree));
+            Debug.Log(GetLeafCount(octree));
+        }
+    }
+
+    void OnDestroy()
+    {
+        FreeOctree(octree);
+    }
+}
---
+++ GaiaDisplayApp/Assets/TestWrapper.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class TestWrapper : MonoBehaviour {
+
+    [DllImport("libtest", EntryPoint = "init_node", CharSet = CharSet.Unicode)]
+    public static extern IntPtr init_node();
+
+    [DllImport("libtest", EntryPoint = "free_node", CharSet = CharSet.Unicode)]
+    public static extern void free_node([In, Out] IntPtr node);
+
+    [DllImport("libtest", EntryPoint = "node_get_a", CharSet = CharSet.Unicode)]
+    public static extern int node_get_a([In, Out] IntPtr node);
+
+    [DllImport("libtest", EntryPoint = "node_get_b", CharSet = CharSet.Unicode)]
+    public static extern float node_get_b([In, Out] IntPtr node);
+
+    [DllImport("libtest", EntryPoint = "test_matrix", CharSet = CharSet.Unicode)]
+    public static extern int test_matrix([In, Out] int[,] matrix, int dimensions);
+
+    [DllImport("libtest", EntryPoint = "test_get_array", CharSet = CharSet.Unicode)]
+    public static extern IntPtr test_get_array();
+
+    [DllImport("libtest", EntryPoint = "test_delete_array", CharSet = CharSet.Unicode)]
+    public static extern void test_delete_array(IntPtr arr);
+
+    [DllImport("libtest", EntryPoint = "array_of_structs", CharSet = CharSet.Unicode)]
+    public static extern void array_of_structs(Position[] position, int length);
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct Position
+    {
+        public float x;
+        public float y;
+        public float z;
+
+        public Position(float x, float y, float z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+    }
+
+    IntPtr node;
+
+    // Use this for initialization
+    void Start () {
+        node = init_node();
+	}
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            Debug.Log(node_get_a(node));
+            Debug.Log(node_get_b(node));
+        }
+
+        if (Input.GetMouseButtonDown(1)) {
+            //float[,] arr = { { 0, 1, 2 }, { 3, 4, 5 }, { 6, 7, 8 } };
+            //Debug.Log(test_double_pointer(arr));
+
+            Position[] positions = new Position[5];
+            for (int i = 0; i < positions.Length; i++) {
+                positions[i].x = UnityEngine.Random.Range(0, 10f);
+                positions[i].y = UnityEngine.Random.Range(0, 10f);
+                positions[i].z = UnityEngine.Random.Range(0, 10f);
+            }
+
+            array_of_structs(positions, positions.Length);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        free_node(node);
+    }
+}
